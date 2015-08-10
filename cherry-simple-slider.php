7db92b1f38da2a87bc941984e3eecb8209d8dd0e
makhonkodenis <behaart@corp.devoffice.com>
2015-08-10 16:16:13 +0300

    Fix base update class

--- cherry-simple-slider.php
+++ cherry-simple-slider.php
@@ -3,12 +3,12 @@
  * Plugin Name: Cherry Simple Slider
  * Plugin URI:  http://www.cherryframework.com/
  * Description: A Slider plugin for WordPress.
- * Version:     1.0.1 beta
+ * Version:     1.0.1
  * Author:      Cherry Team
  * Author URI:  http://www.cherryframework.com/
  * Text Domain: cherry-simple-slider
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:     GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  * Domain Path: /languages
  */
 
